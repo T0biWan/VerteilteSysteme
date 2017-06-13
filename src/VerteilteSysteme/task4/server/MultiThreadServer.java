8ceda97b73b169f8b1fbe194898763a6eb43e60e
t0biwan <tobi.klatt@web.de>
2017-06-13 14:29:32 +0200

    Refactored Server

--- src/VerteilteSysteme/task4/server/MultiThreadServer.java
+++ src/VerteilteSysteme/task4/server/MultiThreadServer.java
@@ -1,36 +1,62 @@
 package task4.server;
 
 import java.io.IOException;
-import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class MultiThreadServer implements Runnable {
-    Socket csocket;
-    MultiThreadServer(Socket csocket) {
-        this.csocket = csocket;
-    }
-    public static void main(String args[]) throws Exception {
-        ServerSocket ssock = new ServerSocket(8090);
-        System.out.println("Listening");
+    private Socket clientSocket;
+    private PrintWriter output;
+    private static int connectedClients = 0;
+    private static int maxConnectedClients = 5;
 
-        while (true) {
-            Socket sock = ssock.accept();
-            System.out.println("Connected");
-            new Thread(new MultiThreadServer(sock)).start();
+    MultiThreadServer(Socket clientSocket) {
+        try {
+            this.clientSocket = clientSocket;
+            this.output = new PrintWriter(clientSocket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
+    public static void main(String args[]) throws Exception {
+        final int port = 8090;
+        final String IP = "127.0.0.1";
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server is running on " + IP + " and listens on Port: " + port);
+
+        while (true) {
+            if (!reachedMaximumOfConnectedClients()) {
+                connectedClients++;
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected [" + connectedClients + "/" + maxConnectedClients + "]");
+                new Thread(new MultiThreadServer(socket)).start();
+            } else {
+                // todo: Wie Client Rückmeldung geben?
+                System.out.println("To many clients");
+            }
+        }
+    }
+
     public void run() {
         try {
-            PrintStream pstream = new PrintStream(csocket.getOutputStream());
-            pstream.println("Fuck you!");
-            for (int i = 100; i >= 0; i--) {
-                pstream.println(i + " bottles of beer on the wall");
-            }
-            pstream.close();
-            csocket.close();
+
+            send("Welcome");
+
+            this.output.close();
+            this.clientSocket.close();
+            connectedClients --;
         } catch (IOException e) {
             System.out.println(e);
         }
+    }
+
+    private void send(String message) {
+        this.output.println(message);
+    }
+
+    private static boolean reachedMaximumOfConnectedClients() {
+        return !(connectedClients <= maxConnectedClients);
     }
 }
