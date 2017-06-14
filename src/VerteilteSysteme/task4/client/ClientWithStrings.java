6e8c362e67a3cbc9c6517ad0c038c15946eb97d5
Tobi Wan <Tobi.Klatt@web.de>
2017-06-14 19:21:19 +0200

    Implemented time() Enhanced Server handling of requests

--- src/VerteilteSysteme/task4/client/ClientWithStrings.java
+++ src/VerteilteSysteme/task4/client/ClientWithStrings.java
@@ -3,66 +3,89 @@ package task4.client;
 import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ClientWithStrings {
-    private static String IP = "127.0.0.1";
-    private static int port = 8090;
-    private static boolean isRunning = true;
-    private static Scanner scanner = new Scanner(System.in);
-    private static String prompt = "$> ";
-    private static Socket socket;
-    private static PrintWriter outputStream;
-    private static BufferedReader inputStream;
+   private static String IP = "127.0.0.1";
+   private static int port = 8090;
+   private static boolean isRunning = true;
+   private static Scanner scanner = new Scanner(System.in);
+   private static String prompt = "$> ";
+   private static Socket socket;
+   private static PrintWriter outputStream;
+   private static BufferedReader inputStream;
 
-    public static void main(String[] args) throws IOException {
+   public static void main(String[] args) throws IOException {
 
 
-        try {
-            socket = new Socket(IP, port);
-            outputStream = new PrintWriter(socket.getOutputStream(), true);
-            inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      try {
+         socket = new Socket(IP, port);
+         outputStream = new PrintWriter(socket.getOutputStream(), true);
+         inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            System.out.println(inputStream.readLine());
-            while (isRunning) processUserInput(getUserInput());
+         System.out.println(inputStream.readLine());
+         while (isRunning) processUserInput(getUserInput());
 
-            inputStream.close();
-            outputStream.close();
-            socket.close();
-            scanner.close();
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + IP);
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " + IP);
-            System.exit(1);
-        }
-    }
+         inputStream.close();
+         outputStream.close();
+         socket.close();
+         scanner.close();
+      } catch (UnknownHostException e) {
+         System.err.println("Don't know about host " + IP);
+         System.exit(1);
+      } catch (IOException e) {
+         System.err.println("Couldn't get I/O for the connection to " + IP);
+         System.exit(1);
+      }
+   }
 
-    private static String getUserInput() {
-        System.out.print(prompt);
-        return scanner.nextLine();
-    }
+   private static String getUserInput() {
+      System.out.print(prompt);
+      return scanner.nextLine();
+   }
 
-    private static void processUserInput(String userInput) throws IOException {
-        if (userInput.equals("e")) end();
-        else {
-            send(userInput);
-            System.out.println(receive());
-        }
-        System.out.println();
-    }
+   private static void processUserInput(String userInput) throws IOException {
+      // Todo Sicherheitsabfrage, userinput darf nciht null sein, genauso wenig wie command und arguments
+      List<String> tokenisedInput = tokenise(userInput);
+      String command = tokenisedInput.get(0);
 
-    private static void send(String message) {
-        outputStream.println(message);
-    }
+      if (command.equals("logout")) end();
+      else if (command.equals("login")) login(tokenisedInput.get(1));
+      else if (command.equals("time")) time();
+      else {
+         send(userInput);
+         System.out.println(receive());
+      }
+      System.out.println();
+   }
 
-    private static String receive() throws IOException {
-        return inputStream.readLine();
-    }
+   private static void send(String message) {
+      outputStream.println(message);
+   }
 
-    private static void end() {
-        System.out.println("goodBye");
-        isRunning = false;
-    }
+   private static String receive() throws IOException {
+      return inputStream.readLine();
+   }
+
+   private static void end() {
+      System.out.println("goodBye");
+      isRunning = false;
+   }
+
+   private static void login(String argument) throws IOException {
+      send(argument);
+      System.out.println(receive());
+   }
+
+   private static List<String> tokenise(String string) {
+      return new ArrayList<String>(Arrays.asList(string.split(" ")));
+   }
+
+   private static void time() throws IOException {
+      send("time");
+      System.out.println(receive());
+   }
 }
--- src/VerteilteSysteme/task4/server/MultiThreadServer.java
+++ src/VerteilteSysteme/task4/server/MultiThreadServer.java
@@ -6,91 +6,99 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MultiThreadServer implements Runnable {
-    private Socket clientSocket;
-    private PrintWriter output;
-    private BufferedReader input;
-    private static int connectedClients = 0;
-    private static int maxConnectedClients = 5;
-    private static ArrayList<String> clients = new ArrayList<>();
+   private Socket clientSocket;
+   private PrintWriter output;
+   private BufferedReader input;
+   private static int connectedClients = 0;
+   private static int maxConnectedClients = 5;
+   private static ArrayList<String> clients = new ArrayList<>();
 
-    MultiThreadServer(Socket clientSocket) {
-        try {
-            this.clientSocket = clientSocket;
-            BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            this.input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            this.output = new PrintWriter(clientSocket.getOutputStream(), true);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String args[]) throws Exception {
-        final int port = 8090;
-        final String IP = "127.0.0.1";
-        ServerSocket serverSocket = new ServerSocket(port);
-        System.out.println("Server is running on " + IP + " and listens on Port: " + port);
-
-        while (true) {
-            if (!reachedMaximumOfConnectedClients()) {
-                Socket socket = serverSocket.accept();
-                connectedClients++;
-                System.out.println("New client connected [" + connectedClients + "/" + maxConnectedClients + "]");
-                new Thread(new MultiThreadServer(socket)).start();
-            } else {
-                // todo: Wie Client Rückmeldung geben?
-                System.out.println("To many clients");
-            }
-        }
-    }
-
-    public void run() {
-        try {
-           welcome();
-           echo();
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-    }
-
-    private void send(String message) {
-        this.output.println(message);
-    }
-
-    private String receive() throws IOException {
-        return input.readLine();
-    }
-
-    private void welcome() {
-       send("Welcome");
-    }
-
-   private void echo() throws IOException {
-      String inputLine;
-      while ((inputLine = input.readLine()) != null) {
-         output.println(inputLine);
+   MultiThreadServer(Socket clientSocket) {
+      try {
+         this.clientSocket = clientSocket;
+         BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+         this.input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+         this.output = new PrintWriter(clientSocket.getOutputStream(), true);
+      } catch (IOException e) {
+         e.printStackTrace();
       }
    }
 
-    private static boolean reachedMaximumOfConnectedClients() {
-        return !(connectedClients <= maxConnectedClients);
-    }
+   public static void main(String args[]) throws Exception {
+      final int port = 8090;
+      final String IP = "127.0.0.1";
+      ServerSocket serverSocket = new ServerSocket(port);
+      System.out.println("Server is running on " + IP + " and listens on Port: " + port);
 
-    private void loginClient() {
-        send("Please enter your username");
-        try {
-            System.out.println(receive());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+      while (true) {
+         if (!reachedMaximumOfConnectedClients()) {
+            Socket socket = serverSocket.accept();
+            connectedClients++;
+            System.out.println("New client connected [" + connectedClients + "/" + maxConnectedClients + "]");
+            new Thread(new MultiThreadServer(socket)).start();
+         } else {
+            // todo: Wie Client Rückmeldung geben?
+            System.out.println("To many clients");
+         }
+      }
+   }
 
-    private void logoutClient() throws IOException {
-        this.output.close();
-        this.clientSocket.close();
-        connectedClients --;
-        System.out.println("Client logged out [" + connectedClients + "/" + maxConnectedClients + "]");
-    }
+   public void run() {
+      try {
+         welcome();
+         processRequest();
+      } catch (IOException e) {
+         System.out.println(e);
+      }
+   }
+
+   private void send(String message) {
+      this.output.println(message);
+   }
+
+   private String receive() throws IOException {
+      return input.readLine();
+   }
+
+   private void welcome() {
+      send("Welcome");
+   }
+
+   private void processRequest() throws IOException {
+      String inputLine;
+      while ((inputLine = input.readLine()) != null) {
+         if (inputLine.equals("time")) time();
+         else send(inputLine);
+      }
+   }
+
+   private static boolean reachedMaximumOfConnectedClients() {
+      return !(connectedClients <= maxConnectedClients);
+   }
+
+   private void loginClient() {
+      send("Please enter your username");
+      try {
+         System.out.println(receive());
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
+
+   private void logoutClient() throws IOException {
+      this.output.close();
+      this.clientSocket.close();
+      connectedClients--;
+      System.out.println("Client logged out [" + connectedClients + "/" + maxConnectedClients + "]");
+   }
+
+   private void time() {
+      Date date = new Date();
+      send(String.format("Current Time: %tc", date));
+   }
 }
